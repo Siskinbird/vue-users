79d03abc4c2bad53d059c6cbb1651fe163b952a1
Dmitrii Chizhov <dimachig@gmail.com>
2022-12-15 11:27:45 +0300

    add: Sort name, age, gender

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -8,15 +8,15 @@
           <!--table head-->
           <thead>
           <tr>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Gender</th>
+            <th @click="sort('name')">Name &#8595;	</th>
+            <th @click="sort('age')">Age &#8595;	</th>
+            <th @click="sort('gender')">Gender &#8595;	</th>
           </tr>
           </thead>
 
           <!--table body-->
           <tbody>
-          <tr v-for="user in users" :key="user.id">
+          <tr v-for="user in usersSort" :key="user.id">
             <td>
               <img :src="user.img" :alt="user.name">
               <span>{{user.name}}</span>
@@ -27,6 +27,15 @@
           </tbody>
 
         </table>
+        <p>debug: sort: {{ currentSort }}, dir: {{ currentSortDir }} </p>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <div class="button-list">
+          <button class="btn btnPrimary"> &#8610; </button>
+          <button class="btn btnPrimary"> &#8611; </button>
+        </div>
       </div>
     </section>
   </div>
@@ -37,7 +46,9 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      users: []
+      users: [],
+      currentSort: 'name',
+      currentSortDir: 'asc'
     }
   },
   created() {
@@ -45,6 +56,32 @@ export default {
         .get("https://tocode.ru/static/_secret/courses/1/usersCrmApi.php")
         .then(response => this.users = response.data)
         .catch(err => console.log(err))
+  },
+  computed: {
+    usersSort () {
+      return this.users.sort((a, b) => {
+        let mod = 1
+        if (this.currentSortDir === 'desc') {
+          mod = -1
+        }
+        if (a[this.currentSort] < b[this.currentSort]) {
+          return -1 * mod
+        }
+        if (a[this.currentSort] > b[this.currentSort]) {
+          return mod
+        }
+        return 0
+
+      })
+    }
+  },
+  methods: {
+    sort(e) {
+      if (e === this.currentSort) {
+        this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc'
+      }
+      this.currentSort = e
+    }
   }
 }
 </script>
